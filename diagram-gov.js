416f568b321ea0945c06de6129548631e2eaa1be
rain <rainbreak@riseup.net>
2017-04-27 22:24:17 +0100

    more gov

--- diagram-gov.js
+++ diagram-gov.js
@@ -10,21 +10,46 @@ document.write(`<div style="float: left; margin-left: 2rem">${Viz(`
     fontname  = "sans-serif";
     fontsize  = 12;
 
-    <fdn> [label=stiftung shape=triangle]
+    <fdn> [label=stiftung shape=triangle height=1.2]
     <dev> [shape=doublecircle]
-    <dss> [label=DSS shape=square style=bold]
 
-    <gov> [label=prism, shape=triangle]
-    <dlg> [label=delegates, shape=doublecircle]
+    <gov> [label=prism shape=square style=bold height=1]
+    <dlg> [label=delegates shape=doublecircle]
 
-    <mkr> [label=MKR shape=doublecircle]
-    <dai> [label=DAI shape=doublecircle]
+    <mkr> [label=MKR shape=circle peripheries=3]
+    <dai> [label=DAI shape=circle peripheries=3]
 
-    <dss> -> <dai>
-    <dss> -> <mkr>
+    subgraph cluster_DSS {
+      fontname = "sans-serif"
+      label    = "Dai StableCoin System"
+      labelloc = bottom
+      style    = bold
+      color    = lightgrey
+
+      node [style=bold]
+      <vox> [shape=square height=0.6]
+      <vat> [shape=square height=0.6]
+      <vow> [shape=square height=0.6]
+    }
+
+    <vat> -> <dai> [label=provides]
+
+    <fdn> -> <dev> [label=hires]
 
     <mkr> -> <gov> [label=votes]
     <gov> -> <dlg> [label=elects]
-    <dlg> -> <dss> [label=adjusts]
+
+    <rats> [shape=square peripheries=2]
+    <rat>  [shape=square style=bold]
+    <dlg>  -> <rats> [label=selects]
+    <rats> -> <rat>  [label=combine]
+    <rat>  -> <vox>  [label=tell]
+
+    <vow> -> <mkr> [label="market incentives"]
+
+    edge [style=dashed minlen=2]
+    <dlg> -> <vox> [label="adjust: sensitivity"]
+    <dlg> -> <vat> [label="adjust: risk params"]
+    <dlg> -> <vow> [label="adjust: liquidation"]
   }
 `, { engine: "dot" })}</div>`)
